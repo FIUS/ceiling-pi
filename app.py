9c303094e53d64886747271cdd71e04f9f3a1bf2
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-11-16 16:57:50 +0100

    Added threading test

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+import time
+import threading
+
 app=Flask(__name__)
+
+def temp():
+    while True:
+        print("haha")
+        print("haha")
+        time.sleep(2)
+
+threading.Thread(target=temp).start()
 
 @app.route("/")
 def hello():
@@ -7,3 +18,4 @@ def hello():
     return "Hi"
 
 app.run(host="0.0.0.0")
+
