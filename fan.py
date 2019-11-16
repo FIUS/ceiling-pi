5d0f43e1009c2a32c5a75fe4b6545eab7c5359f5
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-11-16 16:14:43 +0100

    Added fan script

---
+++ fan.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import psutil as psu
+import time
+while True:
+    cpuTemp=psu.sensors_temperatures()['cpu-thermal'][0][1]
+    print("CPU Temp: "+str(cpuTemp)+" °C")
+    if cpuTemp>60:
+        GPIO.output(5, GPIO.HIGH)
+    elif cpuTemp<40:
+        GPIO.output(5, GPIO.LOW)
+    time.sleep(5*60)
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt upgrade
 sudo apt install python3-pip
 sudo apt install python3-rpi.gpio
+sudo apt install python3-psutil
