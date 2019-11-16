c749ae9e77e63e878421cd73fc16b25623734f7d
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-11-16 17:09:05 +0100

    Speedcheck

--- led_main.py
+++ led_main.py
@@ -1,7 +1,11 @@
 import time
+temp = int(round(time.time() * 1000))
 
 def loop():
+    global temp
     while True:
+        print(int(round(time.time() * 1000))-temp)
+        temp =int(round(time.time() * 1000))
         print("haha")
         print("haha")
         print("looploop")
