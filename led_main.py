4f96742540de9dc42e299876993f197271337b89
schieljn <schieljn@fius.informatik.uni-stuttgart.de>
2019-11-18 00:51:29 +0100

    Implemented printer

--- led_main.py
+++ led_main.py
@@ -1,12 +1,14 @@
 import time
 from neo import *
-import plainColor
+import printer
 import off
 import on
+import plainColor
 
 led_state={
     'type':0,
-    'color' : [0,50,0]
+    'color' : [0,50,0],
+    'printer-color':0
 }
 
 fps = None
@@ -30,6 +32,7 @@ def loop():
             led_functions[led_state['type']](strip,led_state)
             if led_state['type'] % 2==0:
                 led_state['type']+=1
+            printer.magicOverride(strip, led_state)
             strip.show()
         else:
             time.sleep(0.01)
---
+++ printer.py
@@ -0,0 +1,37 @@
+from neopixel import *
+import colorMagic as cm
+printerStart=400
+printerTemp=0
+printerOn=True
+tempColor = [0 for i in range(883-852)]
+
+def magicOverride(strip, data):
+    global printerStart
+    global printerTemp
+    global printerOn
+    global tempColor
+
+    if printerStart > 0:
+        if printerTemp < 0:
+            printerOn = not printerOn
+      
+            if printerOn:
+                for i in range(852,883):
+                    tempColor[i - 852] = strip.getPixelColor(i)
+                    strip.setPixelColor(i,cm.hsv(data['printer-color'],1,1))
+        
+            else:
+                for i in range(852,883):
+                    strip.setPixelColor(tempColor[i - 852])
+            
+            printerTemp = 23
+
+        else:
+            printerTemp-=1
+    
+        printerStart-=1
+    else:
+        if printerOn:
+            printerOn=False
+            for i in range(852,883):
+                strip.setPixelColor(i,tempColor[i - 852])
