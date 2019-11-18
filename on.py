bb4021c3ba7e0eaed5fcbdc62182bb025d5fc231
schieljn <schieljn@fius.informatik.uni-stuttgart.de>
2019-11-18 02:05:23 +0100

    changed brightness cap

--- on.py
+++ on.py
@@ -18,9 +18,9 @@ def update(strip, data):
     if state<255:
         for i in range(0,data['num_pixel']):
             if i > 22 and i < 115 or i > 192 and i < 300:
-                strip.setPixelColor(i, cm.hsv(0.16,0,(state/255.0+brightnessCap)))
+                strip.setPixelColor(i, cm.hsv(0.16,0,state/(255.0+brightnessCap)))
             else:
-                strip.setPixelColor(i, cm.hsv(0.16,1,(state/255.0+brightnessCap)))
+                strip.setPixelColor(i, cm.hsv(0.16,1,state/(255.0+brightnessCap)))
         state+=speed
         if state>255:
             state=255
