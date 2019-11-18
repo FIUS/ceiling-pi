b09c969751c9c909afe92adcdf172db9bfebc767
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-11-18 16:14:58 +0100

    Bigger size of binary counter

--- binaryCounter.py
+++ binaryCounter.py
@@ -3,7 +3,7 @@ from neopixel import *
 
 state = 0
 speed = 5
-
+binarySize=5
 
 def init(strip, data):
     global state
@@ -17,15 +17,18 @@ def update(strip, data):
     global speed
     zahl = "{0:b}".format(state)
 
-    for i in range(0, data['num_pixel']):
+    for i in range(0, int(data['num_pixel']/binarySize)):
         if len(zahl)>0:
             x = int(zahl[len(zahl)-1:len(zahl)])
             zahl = zahl[:len(zahl)-1]
             if x == 0:
-                strip.setPixelColor(i, cm.rgb(0, 0, 0))
+                for f in range(0,binarySize):
+                    strip.setPixelColor(i*binarySize+f, cm.rgb(0, 0, 0))
             else:
-                strip.setPixelColor(i, cm.rgb(data['color'][0],data['color'][1],data['color'][2]))
+                for f in range(0,binarySize):
+                    strip.setPixelColor(i*binarySize+f, cm.rgb(data['color'][0],data['color'][1],data['color'][2]))
         else:
-            strip.setPixelColor(i, cm.rgb(0, 0, 0))
+            for f in range(0,binarySize):
+                strip.setPixelColor(i*binarySize+f, cm.rgb(0, 0, 0))
 
     state += speed
