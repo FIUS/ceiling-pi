9209cbc51617532dc7f8ee8c9c911e352f2df170
schieljn <schieljn@fius.informatik.uni-stuttgart.de>
2019-11-18 02:01:08 +0100

    changed brightness cap

--- on.py
+++ on.py
@@ -2,7 +2,7 @@ import colorMagic as cm
 from neopixel import *
 state=0
 speed=5
-brightnessCap=0.2
+brightnessCap=50
 
 def init(strip, data):
     global state
@@ -15,9 +15,9 @@ def update(strip, data):
     if state<255:
         for i in range(0,data['num_pixel']):
             if i > 22 and i < 115 or i > 192 and i < 300:
-                strip.setPixelColor(i, cm.hsv(0.18,0,(state/255.0)-brightnessCap))
+                strip.setPixelColor(i, cm.hsv(0.18,0,(state/255.0+brightnessCap)))
             else:
-                strip.setPixelColor(i, cm.hsv(0.18,1,(state/255.0)-brightnessCap))
+                strip.setPixelColor(i, cm.hsv(0.18,1,(state/255.0+brightnessCap)))
         state+=speed
         if state>255:
             state=255
