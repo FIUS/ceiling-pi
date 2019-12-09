a9d27c469d04661637067e8701b241af2ea565ad
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-12-10 00:47:23 +0100

    music test

--- music.py
+++ music.py
@@ -1,6 +1,34 @@
 import pyaudio
 import numpy as np
 
+def calculate_levels(data, chunk, samplerate):
+    # Use FFT to calculate volume for each frequency
+    global MAX
+ 
+    # Convert raw sound data to Numpy array
+    fmt = "%dH"%(len(data)/2)
+    data2 = struct.unpack(fmt, data)
+    data2 = numpy.array(data2, dtype='h')
+ 
+    # Apply FFT
+    fourier = numpy.fft.fft(data2)
+    ffty = numpy.abs(fourier[0:len(fourier)/2])/1000
+    ffty1=ffty[:len(ffty)/2]
+    ffty2=ffty[len(ffty)/2::]+2
+    ffty2=ffty2[::-1]
+    ffty=ffty1+ffty2
+    ffty=numpy.log(ffty)-2
+   
+    fourier = list(ffty)[4:-4]
+    fourier = fourier[:len(fourier)/2]
+   
+    size = len(fourier)
+ 
+    # Add up for 6 lights
+    levels = [sum(fourier[i:(i+size/6)]) for i in xrange(0, size, size/6)][:6]
+   
+    return levels
+
 CHUNK = 4096 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
 
@@ -8,10 +36,18 @@ p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 
-# create a numpy array holding a single read of audio data
-for i in range(10): #to it a few times just to see
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    print(data)
+data  = stream.read(chunk)
+ 
+# Do FFT
+levels = calculate_levels(data, chunk, samplerate)
+
+# Make it look better and send to serial
+for level in levels:
+    level = max(min(level / scale, 1.0), 0.0)
+    level = level**exponent
+    level = int(level * 255)
+
+print(levels)
 
 # close the stream gracefully
 stream.stop_stream()
