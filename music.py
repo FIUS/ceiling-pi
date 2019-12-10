bd681363998fdc8b02b60cc970c2a5e4fbb6bfce
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2019-12-10 01:06:31 +0100

    music test

--- music.py
+++ music.py
@@ -42,6 +42,14 @@ CHUNK = 4096 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
 
 p=pyaudio.PyAudio() # start the PyAudio class
+
+info = p.get_host_api_info_by_index(0)
+##Print Audio Devices
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 while(True):
