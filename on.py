422a8a18f984a99af322c4ba52763da54df7f623
schieljn <schieljn@fius.informatik.uni-stuttgart.de>
2020-02-03 21:34:17 +0100

    Added default light chaning to daytime

--- on.py
+++ on.py
@@ -1,8 +1,11 @@
 import colorMagic as cm
 from neopixel import *
+from datetime import datetime
+
 state=0
 speed=5
 brightnessCap=110
+dayColor=0
 
 def init(strip, data):
     global state
@@ -14,13 +17,28 @@ def init(strip, data):
 def update(strip, data):
     global state
     global speed
+    global dayColor
+
+    now = datetime.now()
+    seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+    seconds_since_midnight=seconds_since_midnight**2
+    colorToSet=seconds_since_midnight/7464960000.0
+    colorToSet=int(colorToSet*10000)/10000.0
     
     if state<255:
         for i in range(0,data['num_pixel']):
             if i > 22 and i < 115 or i > 192 and i < 300:
                 strip.setPixelColor(i, cm.hsv(0.14,0,state/(255.0+brightnessCap)))
             else:
-                strip.setPixelColor(i, cm.hsv(0.14,1,state/(255.0+brightnessCap)))
+                strip.setPixelColor(i, cm.hsv(colorToSet,1,state/(255.0+brightnessCap)))
         state+=speed
         if state>255:
             state=255
+    else:
+        if dayColor!=colorToSet:
+            for i in range(0,data['num_pixel']):
+                if i > 22 and i < 115 or i > 192 and i < 300:
+                    strip.setPixelColor(i, cm.hsv(0.14,0,state/(255.0+brightnessCap)))
+                else:
+                    strip.setPixelColor(i, cm.hsv(colorToSet,1,state/(255.0+brightnessCap)))
+            dayColor=colorToSet
