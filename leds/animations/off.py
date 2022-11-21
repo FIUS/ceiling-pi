e0d69bb8696663ac2f3473fa3c29a191bd697047
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-11-21 10:49:30 +0100

    feat(leds/on): Only "turn on" LEDs that were turned off
    This way it is not as problematic if the door is "closed" for a few ms.

--- leds/animations/off.py
+++ leds/animations/off.py
@@ -7,10 +7,12 @@ speed=2
 def init(strip, data):
     global pixelsLeft
     pixelsLeft = [i for i in range(data['num_pixel'])]
+    data["animation_data"]["off"] = {}
+    data["animation_data"]["off"]["pixels_left"] = pixelsLeft
 
 def update(strip, data):
     global pixelsLeft
-    
+
     for i in range(0,speed):
         if len(pixelsLeft)>0:
             offLED=random.randrange(len(pixelsLeft))
